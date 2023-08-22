84e9d23e65d7d59ec9edb4d9f699df78dc43700e
Shubham Acharya <shubham190398@gmail.com>
2023-08-22 12:58:01 +0530

    Dependency injection and read all function

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,8 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from database import engine, SessionLocal
+from typing import Annotated
+from models import Todos
 import models
-from database import engine
 
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.get("/")
+async def read_all(db: Annotated[Session, Depends(get_db)]):
+    return db.query(Todos).all()
