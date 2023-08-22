bb1a3b5768dd8de417187dfab8cea2a165fbc60a
Shubham Acharya <shubham190398@gmail.com>
2023-08-22 20:08:21 +0530

    Adding a post method to create new entries to the database

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -5,6 +5,7 @@ from typing import Annotated
 from models import Todos
 import models
 from starlette import status
+from pydantic import BaseModel, Field
 
 
 app = FastAPI()
@@ -19,6 +20,14 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+
+class TodoRequest(BaseModel):
+    title: str = Field(min_length=3)
+    description: str = Field(min_length=3, max_length=500)
+    priority: int = Field(gt=0, lt=6)
+    complete: bool
 
 
 db_dependency = Annotated[Session, Depends(get_db)]
@@ -37,3 +46,10 @@ async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
         return todo_model
 
     raise HTTPException(status_code=404, detail='Todo not found')
+
+
+@app.post("/todo", status_code=status.HTTP_201_CREATED)
+async def create_todo(db: db_dependency, todo_request: TodoRequest):
+    todo_model = Todos(**todo_request.model_dump())
+    db.add(todo_model)
+    db.commit()
