72b7e90f1f96d34a4b9eb309bc6e97e2c48b8d6b
Shubham Acharya <shubham190398@gmail.com>
2023-08-24 17:44:36 +0530

    Returning a token object instead

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -28,6 +28,11 @@ class CreateUserRequest(BaseModel):
     role: str
 
 
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
 def get_db():
     db = SessionLocal()
 
@@ -75,7 +80,7 @@ async def create_user(db: db_dependency, create_user_request: CreateUserRequest)
     db.commit()
 
 
-@router.post("/token")
+@router.post("/token", response_model=Token)
 async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
                                  db: db_dependency):
     user = authenticate_user(form_data.username, form_data.password, db)
@@ -84,4 +89,4 @@ async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm,
         return "Failed Authentication"
 
     token = create_access_token(user.username, user.id, timedelta(minutes=20))
-    return token
+    return {'access_token': token, 'token_type': 'bearer'}
