f0197c42cfb58a990d2e70ac398891a3a9e10663
Shubham Acharya <shubham190398@gmail.com>
2023-08-24 16:52:50 +0530

    Adding a user to a users database

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -1,7 +1,11 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 from models import Users
 from passlib.context import CryptContext
+from typing import Annotated
+from database import SessionLocal
+from sqlalchemy.orm import Session
+from starlette import status
 
 
 router = APIRouter()
@@ -17,8 +21,20 @@ class CreateUserRequest(BaseModel):
     role: str
 
 
-@router.post("/auth/")
-async def create_user(create_user_request: CreateUserRequest):
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+@router.post("/auth/", status_code=status.HTTP_201_CREATED)
+async def create_user(db: db_dependency, create_user_request: CreateUserRequest):
     create_user_model = Users(
         email=create_user_request.email,
         username=create_user_request.username,
@@ -29,4 +45,5 @@ async def create_user(create_user_request: CreateUserRequest):
         is_active=True
     )
 
-    return create_user_model
+    db.add(create_user_model)
+    db.commit()
