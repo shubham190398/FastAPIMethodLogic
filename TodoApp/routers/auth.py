95c35fecc672cdbe49c247b52d15120ecaf35142
Shubham Acharya <shubham190398@gmail.com>
2023-08-24 18:04:00 +0530

    Cleanup

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -11,13 +11,13 @@ from jose import jwt, JWTError
 from datetime import timedelta, datetime
 
 
-router = APIRouter()
+router = APIRouter(prefix='/auth', tags=['auth'])
 
 SECRET_KEY = '64f29f615d0008690faff1a80044424a'
 ALGORITHM = 'HS256'
 
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
-ouath2_bearer = OAuth2PasswordBearer(tokenUrl='token')
+ouath2_bearer = OAuth2PasswordBearer(tokenUrl='auth/token')
 
 
 class CreateUserRequest(BaseModel):
@@ -80,7 +80,7 @@ async def get_current_user(token: Annotated[str, Depends(ouath2_bearer)]):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Could not validate user')
 
 
-@router.post("/auth/", status_code=status.HTTP_201_CREATED)
+@router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_user(db: db_dependency, create_user_request: CreateUserRequest):
     create_user_model = Users(
         email=create_user_request.email,
@@ -102,7 +102,7 @@ async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm,
     user = authenticate_user(form_data.username, form_data.password, db)
 
     if not user:
-        return "Failed Authentication"
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Could not validate user')
 
     token = create_access_token(user.username, user.id, timedelta(minutes=20))
     return {'access_token': token, 'token_type': 'bearer'}
