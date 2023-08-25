fa4997c2a2790401991866ecf2af52913f03bd67
Shubham Acharya <shubham190398@gmail.com>
2023-08-25 18:59:20 +0530

    Adding none user authentication exception

--- TodoApp/routers/todos.py
+++ TodoApp/routers/todos.py
@@ -33,11 +33,16 @@ user_dependency = Annotated[dict, Depends(get_current_user)]
 
 @router.get("/", status_code=status.HTTP_200_OK)
 async def read_all(db: db_dependency, user: user_dependency):
+    if user is None:
+        raise HTTPException(status_code=401, detail='Authentication Failed')
     return db.query(Todos).filter(Todos.owner_id == user.get('id')).all()
 
 
 @router.get("/todo/{todo_id}", status_code=status.HTTP_200_OK)
-async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
+async def read_todo(db: db_dependency, user: user_dependency, todo_id: int = Path(gt=0)):
+    if user is None:
+        raise HTTPException(status_code=401, detail='Authentication Failed')
+
     todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
 
     if todo_model is not None:
