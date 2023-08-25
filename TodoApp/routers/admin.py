8ab696f85cf7691cc2e0a961f6bcd2674a3789da
Shubham Acharya <shubham190398@gmail.com>
2023-08-25 20:18:13 +0530

    Adding a script for the admin

---
+++ TodoApp/routers/admin.py
@@ -0,0 +1,24 @@
+from fastapi import APIRouter, Depends, HTTPException, Path
+from sqlalchemy.orm import Session
+from database import SessionLocal
+from typing import Annotated
+from models import Todos
+from starlette import status
+from pydantic import BaseModel, Field
+from .auth import get_current_user
+
+
+router = APIRouter(prefix='/admin', tags=['admin'])
+
+
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+user_dependency = Annotated[dict, Depends(get_current_user)]
